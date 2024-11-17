0230b216dbcd8398c7ec446461b5a9a76c5b14d1
Jimmy García <jimmyegc@gmail.com>
2024-11-16 20:09:48 -0600

    11162024

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-How to use AntD's Datepicker with React Hook Form
-
+# How to use AntD's Datepicker with React Hook Form
 https://www.youtube.com/watch?v=rXpBeK8Q4WY
+
+# React Hook Form Controlled Components 1: The process & MUI Checkbox
+https://www.youtube.com/watch?v=3QjUNAUI4JI&t=583s
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,9 @@
 import { useForm } from "react-hook-form";
 import { RHFDatePickerField } from "./RHFDatePickerField/RHFDatePickerField";
+import Checkbox from "@mui/material/Checkbox";
+import { useEffect, useRef, useState } from "react";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { FormControl, FormHelperText } from "@mui/material";
 
 export const Form = () => {
   const {
@@ -11,11 +15,46 @@ export const Form = () => {
   const onSubmit = (data: any) => {
     console.log("Form submitted:", data);
   };
+  
+  const [checked, setChecked] = useState(true);
+  const checkboxRef = useRef<HTMLInputElement>(null)
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+        checkboxRef.current?.focus()
+    }, 1500);
+  },[])
+  
   console.log(errors);
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>            
+    <form onSubmit={handleSubmit(onSubmit)}>   
+      <FormControl sx={{ m:3 }} error component="fieldset" variant="standard">
+        <FormControlLabel    
+          label="Jimmy"    
+          control={(
+            <Checkbox
+              inputRef={checkboxRef}
+              checked={checked}            
+              onChange={handleChange}
+              inputProps={{ 'aria-label': 'controlled' }}
+            />
+          )}
+        />
+        <FormHelperText>You can display an error</FormHelperText>
+      </FormControl>  
+   
+      
+
+      <Checkbox
+        checked={checked}
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'controlled' }}
+      />
       <RHFDatePickerField 
         control={control}
         name="startDate"
@@ -29,6 +68,7 @@ export const Form = () => {
       />
       <br />
       <button type="submit">Submit</button>
+      
     </form>
   );
 };
